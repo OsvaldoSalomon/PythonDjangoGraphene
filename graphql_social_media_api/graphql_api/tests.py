23eb812bc008cc03fd7b43d009a016dbb23b69b5
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-03-06 12:58:09 -0500

    The list of users was created as well as the schemas and tests

--- graphql_social_media_api/graphql_api/tests.py
+++ graphql_social_media_api/graphql_api/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
-
 # Create your tests here.
+from graphene_django.utils import GraphQLTestCase
+
+
+class GraphQLUserTest(GraphQLTestCase):
+    fixtures = ['users.json']
+    def test_retrieve_by_id(self):
+        expected = {
+            "data": {
+                "user": {
+                    "id": "2",
+                    "name": "Dan Bilzerian",
+                    "followers": [
+                        {
+                            "name": "John Doe"
+                        }
+                    ]
+                }
+            }
+        }
+
+        res = self.query("""{
+          user(id:2) {
+            id
+            name
+            followers{
+              name
+            }
+          }
+        }""")
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(expected, res.json())
