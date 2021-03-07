93e341ad4f0568d3a2f787c372a911f679e06570
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-03-07 10:04:41 -0500

    Some tests were created

--- graphql_social_media_api/graphql_api/tests.py
+++ graphql_social_media_api/graphql_api/tests.py
@@ -4,6 +4,7 @@ from graphene_django.utils import GraphQLTestCase
 
 class GraphQLUserTest(GraphQLTestCase):
     fixtures = ['users.json']
+
     def test_retrieve_by_id(self):
         expected = {
             "data": {
@@ -28,6 +29,38 @@ class GraphQLUserTest(GraphQLTestCase):
             }
           }
         }""")
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(expected, res.json())
+
+    def test_create_user(self):
+        expected = {
+            "data": {
+                "createUser": {
+                    "ok": True,
+                    "user": {
+                        "id": "3",
+                        "name": "Elon Musk"
+                    }
+                }
+            }
+        }
+
+        res = self.query(
+            """
+            mutation createUser {
+                createUser(input: {
+                    name: "Elon Musk"
+                }) {
+                    ok
+                    user {
+                        id
+                        name
+                    }
+                }
+            }
+            """
+        )
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(expected, res.json())
