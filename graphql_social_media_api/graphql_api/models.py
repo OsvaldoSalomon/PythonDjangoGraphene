1020a1386c6f89b2abb92b517ac52c62f68afba3
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-03-07 11:52:37 -0500

    More tests and models were added

--- graphql_social_media_api/graphql_api/models.py
+++ graphql_social_media_api/graphql_api/models.py
@@ -5,3 +5,22 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=250)
     followers = models.ManyToManyField('graphql_api.User')
+
+
+class Post(models.Model):
+    content = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+# To retrieve all users with their posts
+# {
+#   users {
+#     id
+#     name
+#     followers{
+#       name
+#     }
+#     postSet {
+#       content
+#     }
+#   }
+# }
